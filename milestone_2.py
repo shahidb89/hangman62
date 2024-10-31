bc3bcefeeee26deb33f27418280d123036a5069b
shahidb89 <shahid_b89@hotmail.com>
2024-10-31 15:20:25 +0000

    Removes redundant code from milestone_2. As no input is needed in milstone_2.

--- milestone_2.py
+++ milestone_2.py
@@ -1,16 +1,6 @@
 import random
 
 word_list = ["apple", "banana", "orange", "plum", "peach"]
-# for word in word_list:
-#     print (word)
 
 word = random.choice(word_list)
 
-# print(word)
-if __name__ ==   "__main__":
-    guess = input("Enter a single letter: ")
-
-    if len(guess) == 1 and guess.isalpha():
-        print('Good guess!')
-    else:
-        print('Oops! That is not a valid input.')
