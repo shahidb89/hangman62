7734e3c354c8dd7b31d9589a1f8369e2bceff9d2
shahidb89 <shahid_b89@hotmail.com>
2024-11-02 13:34:27 +0000

    Corrects sytax errors in the code.

--- milestone_4.py
+++ milestone_4.py
@@ -1,27 +1,35 @@
 import random
 class Hangman:
-    def __init__(self, world_list, num_lives):
-        word.self = random.choice(world_list)
-        word_guessed.self = ['_' for letter in word.self]
-        num_letters.self = len(set(word))
-        num_lives.self = 5
-        world_list.slef = world_list
-        list_of_guesses.self = []
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for letter in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
         
     def check_guess(self, guess):
         guess.lower()
-        if guess in word.self:
+        if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
+            for i, letter in enumerate (self.word):
+                if letter == guess:
+                    self.word_guessed[i] = letter
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} lives lseft')
 
-    def ask_for_input():
+
+    def ask_for_input(self):
         while True:
             guess = input('Enter a single letter: ')
             if not guess.isalpha():
                 print('Invalid letter. Please, enter a single alphabetical character.')
-            elif guess in list_of_guesses.self:
+            elif guess in self.list_of_guesses:
                 print('You already tried that letter!')
             else:
-                check_guess(guess)
-        return list_of_guesses.self.append(guess)
-
-Hangman.ask_for_input()
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
